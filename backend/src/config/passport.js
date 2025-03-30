c7faeeca32ed123095f7d07da69186bf0824aa8f
Dhrutii <thakkardhruti22@gmail.com>
2025-03-30 15:38:50 +0530

    add: passport auth

---
+++ backend/src/config/passport.js
@@ -0,0 +1,51 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import User from "../models/User.js";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+
+        if (!user) {
+          user = new User({
+            googleId: profile.id,
+            name: profile.displayName,
+            email: profile.emails[0].value,
+          });
+          await user.save();
+        }
+
+        return done(null, user);
+      } catch (error) {
+        return done(error, null);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    console.log("id", id);
+
+    const user = await User.findById(id);
+    if (!user) {
+      return done(null, false); // Handle missing user gracefully
+    }
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+});
+
+export default passport;
