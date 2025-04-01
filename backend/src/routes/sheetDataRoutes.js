5aaf73d5f1a443d833b7dec5931b38922fb72ec9
Dhrutii <thakkardhruti22@gmail.com>
2025-04-01 16:09:18 +0530

    all files deleted

--- backend/src/routes/sheetDataRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { sheetData } from "../controllers/sheetDataController.js";
-import AuthMiddleware from "../middleware/authmiddleware.js";
-
-const router = express.Router();
-
-router.get("/data/:sheetId", AuthMiddleware, sheetData);
